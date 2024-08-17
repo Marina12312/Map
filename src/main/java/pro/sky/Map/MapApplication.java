e3f9cdae93e7e467008849a64b9d88994758b861
piatachka <kislovv95@mail.ru>
2024-08-17 13:07:22 +0300

    1commit

---
+++ src/main/java/pro/sky/Map/MapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
